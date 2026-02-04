ab1eee6f7cc77d4253095d8b7dcced3da9393abc
Brijendra Singh <brijj2006@gmail.com>
2026-02-04 17:58:52 +0800

    Initial commit: Accounts microservice

---
+++ accounts/src/main/java/com/bridge/accounts/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.bridge.accounts.repository;
+
+import com.bridge.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
